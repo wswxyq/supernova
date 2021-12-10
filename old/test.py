c29d9f837f10bf5c9d7e2f643b58f6c28a3e71f2
wus <swyx2017@gmail.com>
2021-12-10 14:06:32 -0600

    clean repo and remove shits

--- old/test.py
+++ old/test.py
@@ -16,15 +16,15 @@ def image_gen(event):
         if event[i,0]%2 == 0:
             imgxz[int(event[i,0]/2), int(event[i, 1])] = event[i, 3]
             if event[i, 4] != 0:
-                print(int(event[i,0]/2), int(event[i, 1]))
+                print('xz', int(event[i,0]/2), int(event[i, 1]), event[i, 2], event[i, 3])
         else:
             imgyz[int((event[i, 0]-1)/2), int(event[i, 1])] = event[i, 3]
             if event[i, 4] != 0:
-                print(int((event[i, 0]-1)/2), int(event[i, 1]))
+                print('yz', int((event[i, 0]-1)/2), int(event[i, 1]), event[i, 2], event[i, 3])
     return imgxz, imgyz, event[:, 4].max()
 
 b=a[a[:, 2].argsort()]
-c=b[330000:340000, :]
+c=b[300000:301000, :]
 print(c.shape[0], 'hits')
 imxz, imyz, max_val = image_gen(c)
 plt.figure(figsize=(10,10))
@@ -35,3 +35,4 @@ plt.figure(figsize=(10,10))
 plt.imshow(imyz.T, cmap='jet')
 plt.title('supernova neutrino number: '+str(c[:, 4].sum())+'\n time window:'+str((c[-1, 2]-c[0, 2]))+'ms', fontsize=20)
 plt.show()
+print(imyz[300:400, 200:300]) 
